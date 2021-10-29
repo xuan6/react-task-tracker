2a1e847d3d0b1afaeba754cdf3e9ae57882e028d
Xuan liu <xuanliu@uw.edu>
2021-10-29 11:22:00 -0700

    adding icon to task

---
+++ my-app/src/components/Task.js
@@ -0,0 +1,13 @@
+import {FaTimes} from 'react-icons/fa'
+
+const Task = ({task}) => {
+    return (
+        <div className='task'>
+            <h3>{task.text}<FaTimes className='cancel-icon'/></h3>
+            <p>{task.day}</p>
+            
+        </div>
+    )
+}
+
+export default Task
